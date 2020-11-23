58626e4d2cbec4bad789b236c556f4abe990c065
mojpm <itsjoelpm@gmail.com>
2020-11-23 10:25:09 +0300

    Update Part.js

--- part2/courseinfo/src/components/Part.js
+++ part2/courseinfo/src/components/Part.js
@@ -1,21 +1,31 @@
-import React from 'react'
-
+import React from 'react';
 
 const Part = ({ course }) => {
-    const part = course.map((course) => {
-        return course.parts.map((part) => {
-            return (
-                <div key={part.id}>
-                    <h2>{part.name}</h2>
-                    <p>
-                        {part.name} {part.exercises}
-                    </p>
-                    <p>Number of exercises {part.exercises}</p>
-                </div>
-            );
-        });
+  const part = course.map((course) => {
+    const courseName = course.name;
+    const courseID = course.id;
+
+    const subCourse = course.parts.map((part) => {
+      return (
+        <div key={part.id}>
+          <h3>{part.name}</h3>
+          <p>
+            {part.name} {part.exercises}
+          </p>
+          <p>Number of exercises {part.exercises}</p>
+        </div>
+      );
     });
-    return <>{part}</>;
+
+    return (
+      <div key={courseID}>
+        <h2>{courseName}</h2>
+        <div>{subCourse}</div>
+      </div>
+    );
+  });
+
+  return <> {part} </>;
 };
 
 export default Part;
