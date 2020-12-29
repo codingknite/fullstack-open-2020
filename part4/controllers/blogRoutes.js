0f1157292a4f760894e1e23e3f3120ca6cc6693d
mojpm <itsjoelpm@gmail.com>
2020-12-29 18:52:37 +0300

    update blog routes

--- part4/controllers/blogRoutes.js
+++ part4/controllers/blogRoutes.js
@@ -1,53 +1,40 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
+blogRouter.get('/', async (request, response) => {
+    const notes = await Blog.find({})
+    response.json(notes)
+})
+
+blogRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    response.json(blog)
+
+})
+
+blogRouter.put('/:id', async (request, response) => {
+    await Blog.findByIdAndUpdate(request.params.id)
+    response
+        .status(201)
+        .json({
+            message: "Blog updated successfully"
         })
 })
 
-blogRouter.get('/:id', (request, response) => {
-    Blog
-        .findById(request.params.id)
-        .then(blog => {
-            response.json(blog)
-        })
+blogRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response
+        .status(204)
+        .end()
 })
 
-blogRouter.put('/:id', (request, response) => {
-    Blog
-        .findByIdAndUpdate(request.params.id)
-        .then(() => {
-            response
-                .status(201)
-                .json({
-                    message: "Blog updated successfully"
-                })
-        })
-})
-
-blogRouter.delete('/:id', (request, response) => {
-    Blog
-        .findByIdAndRemove(request.params.id)
-        .then(() => {
-            response
-                .status(204)
-                .end()
-        })
-})
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(savedBlog => {
-            response
-                .status(201)
-                .json(savedBlog)
-        })
+    const savedBlog = await blog.save()
+    response
+        .status(201)
+        .json(savedBlog)
 })
 
 module.exports = blogRouter
