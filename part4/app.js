ad4172601473bd4804c543196c257325c63a2d6b
mojpm <itsjoelpm@gmail.com>
2021-05-11 18:54:18 +0300

    update app config

--- part4/app.js
+++ part4/app.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require('express-async-errors')
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
 const config = require('./utils/config')
-const logger = require('./utils/loggers')
-const blogRouter = require('./controllers/blogRoutes')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
-const url = config.MONGODB_URI
+mongoose.set('useCreateIndex', true)
 
-mongoose
-    .connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true,
-        useCreateIndex: false
-    })
-    .then(() => {
-        logger.info('connected to MONGODB')
-    })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+mongoose.set('useFindAndModify', false)
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/blogs', blogRouter)
+app.use(middleware.tokenExtractor)
 
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-module.exports = app
+app.use(middleware.errorHandler)
+
+module.export
