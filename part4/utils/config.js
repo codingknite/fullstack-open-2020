91ae407b608b1c26755dff28ead5bf30662153bc
mojpm <itsjoelpm@gmail.com>
2021-05-11 19:19:50 +0300

    minor config updates

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
+let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
-const PORT = process.env.PORT
 
 if (process.env.NODE_ENV === 'test') {
-    MONGODB_URI = process.env.TEST_MONGODB_URI
+  MONGODB_URI = process.env.TEST_MONGODB_URI
 }
+
 module.exports = {
-    MONGODB_URI,
-    PORT
+  MONGODB_URI,
+  PORT
 }
