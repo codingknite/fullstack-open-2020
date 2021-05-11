d53205d8656e468c30325525f4b62254b074dfa8
mojpm <itsjoelpm@gmail.com>
2021-05-11 18:59:49 +0300

    add intial app config

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,33 +3,33 @@ const router = require('express').Router()
 const User = require('../models/user')
 
 router.get('/', async (request, response) => {
-    const users = await User
-        .find({})
-        .populate('blogs', { title: 1, url: 1, likes: 1, author: 1 })
+  const users = await User
+    .find({})
+    .populate('blogs', { title: 1, url: 1,  likes: 1, author: 1 })
 
-    response.json(users.map(u => u.toJSON()))
+  response.json(users.map(u => u.toJSON()))
 })
 
 router.post('/', async (request, response) => {
-    const { password, name, username } = request.body
+  const { password, name, username } = request.body
 
-    if (!password || password.length < 3) {
-        return response.status(400).send({
-            error: 'password must min length 3'
-        })
-    }
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    const user = new User({
-        username, name,
-        passwordHash,
+  if ( !password || password.length<3 ) {
+    return response.status(400).send({
+      error: 'password must min length 3'
     })
+  }
 
-    const savedUser = await user.save()
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    response.json(savedUser)
+  const user = new User({
+    username, name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
 })
 
 module.exports = router
--- part4/index.js
+++ part4/index.js
@@ -1,7 +1,10 @@
-const config = require('./utils/config')
 const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const PORT = config.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
